f3bc14da43e18dbae60a4cd278784623c23adaa2
Sevval Turkyilmaz <sevvalturkyilmazz@gmail.com>
2020-12-26 00:18:37 +0300

    First commit

---
+++ src/DescendingArrays.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class DescendingArrays {
+    static Scanner scan;
+    public static int i;
+
+    public static int[] getInteger(int n) {
+        scan = new Scanner(System.in);
+        int [] numbers=new int[n];
+        System.out.println("Please enter the numbers");
+        for (i = 0; i < n; i++) {
+            numbers[i] = scan.nextInt();
+        }
+        return numbers;
+    }
+
+    public static void printInteger(int[] printNumbers) {
+        for (i = 0; i < printNumbers.length; i++) {
+            System.out.print(printNumbers[i]+" ");
+        }
+        System.out.println("");
+    }
+    public static int[] sortArray(int[]ints){
+        int []sortedArray=new int[ints.length];
+       for (int i=0;i<ints.length;i++)
+       {
+           sortedArray[i]=ints[i];
+       }
+       boolean flag=true;
+       int temp;
+       while(flag){
+           flag=false;
+           int maxIndex=sortedArray.length-1;
+           for (int i=0;i<maxIndex;i++){
+               if (sortedArray[i]<sortedArray[i+1])
+               {
+                    temp=sortedArray[i];
+                    sortedArray[i]=sortedArray[i+1];
+                    sortedArray[i+1]=temp;
+                    flag=true;
+               }
+           }
+       }
+
+        return sortedArray;
+    }
+
+    public static void main(String[] args) {
+        scan =new Scanner(System.in);
+        System.out.println("Please enter the array length");
+        int y=scan.nextInt();
+        int [] firstArray=getInteger(y);
+        System.out.println("This is your array");
+        printInteger(firstArray);
+        int [] sortedArrray= sortArray(firstArray);
+        System.out.println("This is the array in descending order");
+        printInteger(sortedArrray);
+
+
+    }
+}
---
+++ src/ReverseArray.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please type the array length");
+        int length= scanner.nextInt();
+        if (length<0){
+            System.out.println("Please enter positive value");
+        }
+        int []arr=new int[length];
+        System.out.println("Please fill the array");
+        for (int i=0;i<length;i++){
+            arr[i]=scanner.nextInt();
+        }
+        System.out.println("Diziniz : "+Arrays.toString(arr));
+        reverseArray(arr);
+        System.out.println("Dizinin ters şekli : "+Arrays.toString(arr));
+    }
+    private static void reverseArray(int[] array)
+    {
+        int maxIndex= array.length-1;
+        int halfIndex=array.length/2;
+         for (int i=0;i<halfIndex;i++)
+         {
+             int temp =array[i];
+             array[i]=array[maxIndex-i];
+             array[maxIndex-i]=temp;
+         }
+
+    }
+}
